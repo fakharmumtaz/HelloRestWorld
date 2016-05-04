c14d272fce032008461c887105f7f7e0f83f91ed
fakharmumtaz <fakharmumtaz@gmail.com>
2016-05-03 21:15:25 -0400

    Hello World Web Application commit

---
+++ helloworld/src/main/java/org/mumtaz/helloworld/HelloWorldResource.java
@@ -0,0 +1,17 @@
+package org.mumtaz.helloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("hello")
+public class HelloWorldResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    //@Path("hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
---
+++ helloworld/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+    <h2>RESTful Hello World Web Application!</h2>
+    <p><a href="webapi/helloresource/hello">Hello World Resource</a>    
+</body>
+</html>
